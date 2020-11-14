97a9474a8a67645afbc00b6f4656bbb2dcecc0ba
arielxioxio <71679945+arielxioxio@users.noreply.github.com>
2020-11-15 07:02:09 +0800

    期中1

---
+++ ASIA_2D_Midterm_107051071/Assets/scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    //public void NextLevel()
+    
+        //SceneManager.LoadScene("關卡2");
+    
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("選單");
+
+    }
+    public void Quit()
+    {
+
+    }
+}
